31ab2e91928ed1eb1f3373e53ad10541ef992b2f
corewebsolutions <contact@corewebsolutions.com>
2024-01-03 14:15:57 -0500

    Update user-profile.js

--- components/user-profile.js
+++ components/user-profile.js
@@ -298,8 +298,7 @@ function loadUserProfile(user) {
         $("[data-profile=monthly_rent]")
           .text("$" + response.tenant_info.monthly_rent)
           .val(response.tenant_info.monthly_rent);
-        $("[data-profile=enable_payments]")
-          .val(response.tenant_info.enable_payments);
+        $("[data-profile=enable_payments]").prop("checked", response.tenant_info.enable_payments);
         $("[data-profile=auto_pay_date]")
           .text(response.tenant_info.auto_pay_date)
           .val(response.tenant_info.auto_pay_date);
