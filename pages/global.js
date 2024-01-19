3728a4f867ef67e3ec46ee1b5a7881784b81e8be
corewebsolutions <contact@corewebsolutions.com>
2024-01-19 11:04:56 -0500

    Update global.js

--- pages/global.js
+++ pages/global.js
@@ -55,7 +55,7 @@ function initializeApp() {
         request_data: settings.data ? JSON.stringify(settings.data) : '', // Stringify if the data is an object
         response_data: jqXHR.responseText,
         settings_url: settings.url,
-        user: localStorage.firstName + " " localStorage.lastName
+        user: localStorage.lastName
     });
 
     // Send the error data to your server
