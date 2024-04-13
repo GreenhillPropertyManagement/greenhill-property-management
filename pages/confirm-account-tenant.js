aa21e23783333b464c04bde4cbd837e7b66e1a26
corewebsolutions <contact@corewebsolutions.com>
2024-04-12 22:53:25 -0400

    Update confirm-account-tenant.js

--- pages/confirm-account-tenant.js
+++ pages/confirm-account-tenant.js
@@ -80,6 +80,13 @@ function confirmTenant(){
         localStorage.setItem("userRecId", response.id); // add user record ID
         localStorage.setItem("tenantRecId", response.tenant_info.id); // add user record ID
 
+        // check if they have payments enabled and update the onboarding form accordingly....
+
+        if (response.tenant_info.enable_payments == false) {
+          $('#banking-form-block').remove();
+        }
+        
+
         // check if they have previously entered a bank account already, if so send them to verification page. 
         // this check is for if they go back to this page for a second time after already entering their initial bank account
         // we do this to avoid double entry bank accounts before a verification
