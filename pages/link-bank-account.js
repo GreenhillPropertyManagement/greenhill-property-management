38e5c1af98cfc4c2a20923d08c737c097e0c8006
corewebsolutions <contact@corewebsolutions.com>
2024-05-22 13:11:54 -0400

    Update link-bank-account.js

--- pages/link-bank-account.js
+++ pages/link-bank-account.js
@@ -40,7 +40,7 @@ function createLinkToken() {
   // AJAX request to fetch Link token
   $.ajax({
     type: 'POST',
-    url: localStorage.baseUrl + 'api:WROWQVjv/Create_Link_Token',
+    url: localStorage.baseUrl + 'api:WROWQVjv/create_plaid_link_token',
     contentType: 'application/json',
     success: function(response) {
       // Handle successful response
