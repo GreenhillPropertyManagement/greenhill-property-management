ee2bd703e1cd168b75776b7f49f270818831a802
corewebsolutions <contact@corewebsolutions.com>
2025-04-02 16:14:53 -0400

    Update calendar.js

--- components/calendar.js
+++ components/calendar.js
@@ -44,97 +44,106 @@ function calendarInit() {
     function generateCalendar(events) {
         calendarBody.innerHTML = '';
         calendarTitle.textContent = currentDate.toLocaleString('default', { month: 'long', year: 'numeric' });
-  
+      
         const firstDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
         const lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
         const daysInMonth = lastDay.getDate();
         const daysInPrevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
         const startDayOfWeek = firstDay.getDay();
-  
+      
         const dates = [];
         for (let i = daysInPrevMonth - startDayOfWeek + 1; i <= daysInPrevMonth; i++) {
-            dates.push(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, i));
+          dates.push(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, i));
         }
         for (let i = 1; i <= daysInMonth; i++) {
-            dates.push(new Date(currentDate.getFullYear(), currentDate.getMonth(), i));
+          dates.push(new Date(currentDate.getFullYear(), currentDate.getMonth(), i));
         }
         for (let i = 1; i <= 7 - lastDay.getDay() - 1; i++) {
-            dates.push(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, i));
+          dates.push(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, i));
         }
-  
+      
         if (isMobileDevice()) {
-            const filteredEvents = events.filter(event => {
-                const eventDate = new Date(event.calendar_date);
-                return eventDate.getFullYear() === currentDate.getFullYear() &&
-                    eventDate.getMonth() === currentDate.getMonth();
+          const filteredEvents = events.filter(event => {
+            const eventDate = new Date(event.calendar_date);
+            return eventDate.getFullYear() === currentDate.getFullYear() &&
+                   eventDate.getMonth() === currentDate.getMonth();
+          });
+      
+          filteredEvents.forEach(event => {
+            let eventTemplate;
+      
+            if (event.is_task) {
+                eventTemplate = document.querySelector('.calendar-event-sample-wrapper [data-dyn-item="task-event"]').cloneNode(true);
+              
+                eventTemplate.setAttribute('element', 'modal');
+                eventTemplate.setAttribute('modal', 'new-task');
+              
+            
+                eventTemplate.querySelector('[data-task="title"]').textContent = event.task_title;
+                eventTemplate.querySelector('[data-task="user"]').textContent = event.assigned_user_name.display_name;
+                eventTemplate.querySelector('[data-task="date"]').textContent = formatDateNoTime(event.calendar_date);
+              } else {
+              // Use regular event template
+              eventTemplate = document.querySelector('.calendar-event-sample-wrapper [data-dyn-item="calendar-event"]').cloneNode(true);
+              eventTemplate.querySelector('[data-calendar="type"]').textContent = event.type;
+              eventTemplate.querySelector('[data-calendar="tenant_name"]').textContent = event.display_name;
+              eventTemplate.querySelector('[data-calendar="property_name"]').textContent = event.street;
+              eventTemplate.querySelector('[data-calendar="unit_name"]').textContent = event.unit_name;
+              eventTemplate.querySelector('[data-calendar="date"]').textContent = formatDateNoTime(event.calendar_date);
+            }
+      
+            $(eventTemplate).on('click', function () {
+              $(this).find('.calendar__event__info-wrapper').toggle();
             });
-  
-            filteredEvents.forEach(event => {
-                const eventTemplate = document.querySelector('.calendar-event-sample-wrapper [data-dyn-item="calendar-event"]').cloneNode(true);
-  
-                // Set attributes and content based on task or regular event
-                if (event.is_task) {
-                    eventTemplate.setAttribute('element', 'modal');
-                    eventTemplate.setAttribute('modal', 'new-task');
-                    eventTemplate.querySelector('[data-calendar="type"]').textContent = event.task_title;
-                } else {
-                    eventTemplate.querySelector('[data-calendar="type"]').textContent = event.type;
-                }
-  
+      
+            calendarBody.appendChild(eventTemplate);
+          });
+      
+        } else {
+          // Desktop view
+          for (const date of dates) {
+            const dayDiv = document.querySelector('.calendar__day').cloneNode(true);
+            const dayNumber = dayDiv.querySelector('.calendar__day__number');
+            dayNumber.textContent = date.getDate();
+      
+            const eventsForDay = events.filter(event => {
+              const eventDate = new Date(event.calendar_date);
+              return eventDate.getFullYear() === date.getFullYear() &&
+                     eventDate.getMonth() === date.getMonth() &&
+                     eventDate.getDate() === date.getDate();
+            });
+      
+            for (const event of eventsForDay) {
+              let eventTemplate;
+      
+              if (event.is_task) {
+                eventTemplate = document.querySelector('.calendar-event-sample-wrapper [data-dyn-item="task-event"]').cloneNode(true);
+                eventTemplate.querySelector('[data-task="title"]').textContent = event.task_title;
+                eventTemplate.querySelector('[data-task="user"]').textContent = event.assigned_user_name.display_name;
+                eventTemplate.querySelector('[data-task="date"]').textContent = formatDateNoTime(event.calendar_date);
+              } else {
+                eventTemplate = document.querySelector('.calendar-event-sample-wrapper [data-dyn-item="calendar-event"]').cloneNode(true);
+                eventTemplate.querySelector('[data-calendar="type"]').textContent = event.type;
                 eventTemplate.querySelector('[data-calendar="tenant_name"]').textContent = event.display_name;
                 eventTemplate.querySelector('[data-calendar="property_name"]').textContent = event.street;
                 eventTemplate.querySelector('[data-calendar="unit_name"]').textContent = event.unit_name;
-                eventTemplate.querySelector('[data-calendar="date"]').textContent = formatDateNoTime(event.calendar_date);
-  
-                $(eventTemplate).on('click', function () {
-                    $(this).find('.calendar__event__info-wrapper').toggle();
-                });
-  
-                calendarBody.appendChild(eventTemplate);
-            });
-        } else {
-            for (const date of dates) {
-                const dayDiv = document.querySelector('.calendar__day').cloneNode(true);
-                const dayNumber = dayDiv.querySelector('.calendar__day__number');
-                dayNumber.textContent = date.getDate();
-  
-                const eventsForDay = events.filter(event => {
-                    const eventDate = new Date(event.calendar_date);
-                    return eventDate.getFullYear() === date.getFullYear() &&
-                        eventDate.getMonth() === date.getMonth() &&
-                        eventDate.getDate() === date.getDate();
-                });
-  
-                for (const event of eventsForDay) {
-                    const eventTemplate = document.querySelector('.calendar-event-sample-wrapper [data-dyn-item="calendar-event"]').cloneNode(true);
-  
-                    // Set attributes and content based on task or regular event
-                    if (event.is_task) {
-                        eventTemplate.setAttribute('element', 'modal');
-                        eventTemplate.setAttribute('modal', 'new-task');
-                        eventTemplate.querySelector('[data-calendar="type"]').textContent = event.task_title;
-                    } else {
-                        eventTemplate.querySelector('[data-calendar="type"]').textContent = event.type;
-                    }
-  
-                    eventTemplate.querySelector('[data-calendar="tenant_name"]').textContent = event.display_name;
-                    eventTemplate.querySelector('[data-calendar="property_name"]').textContent = event.street;
-                    eventTemplate.querySelector('[data-calendar="unit_name"]').textContent = event.unit_name;
-  
-                    $(eventTemplate).on('click', function () {
-                        $(this).find('.calendar__event__info-wrapper').toggle();
-                    });
-  
-                    dayDiv.appendChild(eventTemplate);
-                }
-  
-                if (date.getMonth() !== currentDate.getMonth()) {
-                    dayDiv.classList.add('inactive');
-                }
-                calendarBody.appendChild(dayDiv);
+              }
+      
+              $(eventTemplate).on('click', function () {
+                $(this).find('.calendar__event__info-wrapper').toggle();
+              });
+      
+              dayDiv.appendChild(eventTemplate);
             }
+      
+            if (date.getMonth() !== currentDate.getMonth()) {
+              dayDiv.classList.add('inactive');
+            }
+      
+            calendarBody.appendChild(dayDiv);
+          }
         }
-    }
+      }
   
     loadCalendarEvents();
   
